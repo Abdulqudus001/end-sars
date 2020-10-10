4102fa6d3fbe651e384fa91efe38036566e3655a
abdulqudus001 <abdulqudusabubakre@gmail.com>
2020-10-10 10:26:17 +0100

    Added loader

--- src/components/Locations.vue
+++ src/components/Locations.vue
@@ -1,13 +1,50 @@
 <template>
   <div id="locations">
+    <v-snackbar
+      v-model="snackbar"
+    >
+      {{ text }}
+
+      <template v-slot:action="{ attrs }">
+        <v-btn
+          color="primary"
+          text
+          v-bind="attrs"
+          @click="snackbar = false"
+        >
+          Close
+        </v-btn>
+      </template>
+    </v-snackbar>
     Hello All Locations
   </div>
 </template>
 
 <script>
 export default {
+  data: () => ({
+    snackbar: false,
+    text: '',
+  }),
+  mounted() {
+    this.loadLocations();
+  },
+  methods: {
+    loadLocations() {
+      const locations = [];
+      this.$firebase.collection('protests').get().then((snapshot) => {
+        snapshot.forEach((doc) => {
+          locations.push(doc.data());
+        });
+      }).catch(() => {
+        this.snackbar = true;
+        this.text = 'Something went wrong, please refresh';
+      });
 
-}
+      this.locations = locations;
+    },
+  },
+};
 </script>
 
 <style>
