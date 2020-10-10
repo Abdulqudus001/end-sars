099370589512ee9f4d7810abd1c2c8188337d12c
abdulqudus001 <abdulqudusabubakre@gmail.com>
2020-10-10 11:47:10 +0100

    Feat: Added search

--- src/components/Locations.vue
+++ src/components/Locations.vue
@@ -22,14 +22,24 @@
       </div>
     </template>
     <template v-else>
-      <v-row>
+      <v-row justify="center" class="mx-auto search">
+        <v-text-field
+          placeholder="Search Locations"
+          color="red"
+          v-model="search"
+        />
+      </v-row>
+      <v-row v-if="allLocations.length > 0">
         <v-col
-          v-for="location in locations"
+          v-for="location in allLocations"
           :key="location.location.place_id"
           sm="6"
         >
           <Location :location="location" />
         </v-col>
+      </v-row>
+      <v-row v-else justify="center">
+        <h3 class="grey--text no-result">No Results Found</h3>
       </v-row>
     </template>
   </div>
@@ -46,7 +56,24 @@ export default {
     text: '',
     loading: false,
     locations: [],
+    search: '',
   }),
+  computed: {
+    allLocations() {
+      if (this.search.length < 0) {
+        return this.locations;
+      }
+
+      const filtered = this.locations.filter((location) => {
+        const name = location.location.name.toLowerCase();
+        const address = location.location.formatted_address.toLowerCase();
+        const search = this.search.toLowerCase();
+        return name.includes(search) || address.includes(search);
+      });
+
+      return filtered;
+    },
+  },
   mounted() {
     this.loadLocations();
   },
@@ -71,6 +98,15 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss" scoped>
+.search {
+  max-width: 600px;
+}
+.no-result {
+  font-size: 3rem;
 
+  @media screen and (max-width: 768px){
+    font-size: 2rem;
+  }
+}
 </style>
