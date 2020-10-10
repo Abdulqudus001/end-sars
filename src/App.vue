91ad7bf55d33ff11ad7c923c73e823fdef08d55f
abdulqudus001 <abdulqudusabubakre@gmail.com>
2020-10-10 08:32:18 +0100

    Init commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,39 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-app-bar app elevation="2">
+      <div class="d-flex align-center">
+        <h1>#EndSARS</h1>
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn href="https://twitter.com/ibn_abubakre" target="_blank" text icon>
+        <v-icon>mdi-twitter</v-icon>
+      </v-btn>
+      <v-btn
+        href="https://github.com/abdulqudus001/end-sars"
+        target="_blank"
+        text
+        icon
+      >
+        <v-icon>mdi-github</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <Home />
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Home from '@/components/Home.vue';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  components: { Home },
+  data: () => ({
+    //
+  }),
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,31 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import vuetify from './plugins/vuetify';
+import Axios from 'axios';
+import * as VueGoogleMaps from 'vue2-google-maps';
 
-Vue.config.productionTip = false
+const firebase = require('firebase');
+require('firebase/firestore');
+
+firebase.initializeApp({
+  apiKey: process.env.VUE_APP_API_KEY,
+  authDomain: process.env.VUE_APP_AUTH_DOMAIN,
+  projectId: process.env.VUE_APP_PROjECT_ID,
+});
+
+Vue.config.productionTip = false;
+Vue.prototype.$firebase = firebase.firestore();
+
+Vue.prototype.$axios = Axios;
+
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: process.env.VUE_APP_MAP,
+    libraries: 'places',
+  },
+});
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  vuetify,
+  render: (h) => h(App),
+}).$mount('#app');
